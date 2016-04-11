b81903e3b0ae70d26266cab9c2aa011fa7986c47
WoLfulus <wolfulus@gmail.com>
2016-04-11 20:35:01 -0300

    Sparkline test update

--- web/client/components/pages/traffic.js
+++ web/client/components/pages/traffic.js
@@ -3,7 +3,7 @@
  */
 import {Button, Icon, Row, Input}     from 'react-materialize';
 import appHistory                     from 'apphistory'
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesSpots  } from 'react-sparklines';
 
 import 'styles/traffic.scss';
 
@@ -14,10 +14,13 @@ export default class TrafficView extends React.Component {
 
     constructor() {
         super();
-
-        this.state = {
-            data: [5, 10, 5, 20, 8, 15]
+        var d = [];
+        for (var i = 0; i < 32; i++) {
+            d.push(Math.round(25 + (Math.random() * 75)));
         }
+        this.state = {
+            data: d
+        };
     }
 
     handleGoBack = () => {
@@ -25,35 +28,32 @@ export default class TrafficView extends React.Component {
     }
 
     componentDidMount() {
-
-        setTimeout(() => {
+        setInterval(() => {
+            var data = this.state.data.concat(Math.round(25 + (Math.random() * 75)));
+            if (data.length > 64) {
+                data = data.splice(32);
+            }
             this.setState({
-                data: [10, 20, 0, 20, 16, 30]
+                data: data
             });
-        }, 1000);
-
+        }, 500);
     }
 
     /**
      * Render
      */
     render() {
-
         return (
             <div className='grey darken-3 noselect valign-wrapper traffic'>
-
                 <Row className='container row valign center padding-1x'>
-
                     <div className='valign firewow-logo'></div>
-
                     <div>
                         <h4>Traffic View</h4>
-
-                        <Sparklines data={this.state.data} limit={20} width={320} height={300}>
+                        <Sparklines data={this.state.data} limit={32} width={600} height={50}>
                             <SparklinesLine style={{ stroke: "none", fill: "#8e44af", fillOpacity: "1" }}/>
+                            <SparklinesSpots />
                         </Sparklines>
                     </div>
-
                 </Row>
             </div>
         );
