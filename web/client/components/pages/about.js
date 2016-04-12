bdf3e3691ab1a476248490a06f373be198db6d03
Augusto Russo <augustopaladin@gmail.com>
2016-04-12 07:33:22 -0300

    UI changes

--- web/client/components/pages/about.js
+++ web/client/components/pages/about.js
@@ -32,11 +32,23 @@ export default class About extends React.Component {
 
                     <p className="brand">Made with <Icon>favorite</Icon> using</p>
 
-                    <div className='tools'>
+                    <div className='tools z-depth-1'>
 
                         <div className='node'></div>
                         <div className='webpack'></div>
                         <div className='react'></div>
+                        <div className='sass'></div>
+                        <div className='babel'></div>
+                        <div className='bower'></div>
+                        <div className='nodemon'></div>
+                        <div className='npm'></div>
+                        <div className='express'></div>
+
+                        <div className='authors pink white-text'>
+                            <h4>AUTHORS</h4>
+                            <p>Augusto Russo  (augustopaladin@gmail.com)</p>
+                            <p>João F. Biondo (wolfulus@gmail.com)</p>
+                        </div>
 
                     </div>
 
